4951a010718a49065d70abe8905316814b0ad502
ming <1960588251@qq.com>
2021-05-24 14:45:07 +0800

    bubble

---
+++ sort/bubble_sort.go
@@ -0,0 +1,11 @@
+package sort
+
+func BubbleSort(arr []int) {
+	for i := range arr {
+		for j := len(arr) - 1; j > i; j-- {
+			if arr[j] < arr[j-1] {
+				arr[j], arr[j-1] = arr[j-1], arr[j]
+			}
+		}
+	}
+}
---
+++ sort/bubble_sort_test.go
@@ -0,0 +1,20 @@
+package sort
+
+import (
+	"testing"
+)
+
+func TestBubbleSort(t *testing.T) {
+	var arr = []int{5, 7, 3, 2, 7, 55, 33, 55, 13, 56, 758}
+	BubbleSort(arr)
+	t.Log(arr)
+	for k := range arr {
+		if k == 0 {
+			continue
+		}
+		if arr[k-1] > arr[k] {
+			t.Error(arr, "排序失败")
+			break
+		}
+	}
+}
